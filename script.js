3ad021430a712b2e2405fcc6d858279661855eb4
lynn-0416 <275203841+lynn-0416@users.noreply.github.com>
2026-05-22 14:32:05 +0800

    update homepage

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const alertButton = document.getElementById("alertButton");
+
+  alertButton?.addEventListener("click", function () {
+    alert("你好！你已經成功建立基本網站架構。");
+  });
+});
